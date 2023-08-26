5d49058f9402fe10419d8ab82d563630d2d87aca
Jérémy chassin <jerem.chassin@gmail.com>
2023-08-26 13:43:02 +0200

    Modification pour ajouter une vector database in Memory FAISS, changement de la chain en conséquence et modification du loader de text par un loader de pdf multiple

--- main.py
+++ main.py
@@ -2,10 +2,18 @@ import json
 import os
 import openai
 import langchain
-from langchain.document_loaders import TextLoader
 from langchain.chat_models import ChatOpenAI
-from langchain.chains.question_answering import load_qa_chain
+
+from langchain.document_loaders import PyPDFDirectoryLoader
+from langchain.embeddings import OpenAIEmbeddings
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain.vectorstores import FAISS
+
+from langchain.chains import RetrievalQA
+
 from langchain.memory import ConversationBufferWindowMemory
+from langchain.prompts import PromptTemplate
+
 from flask import Flask, request, make_response
 from flask_cors import CORS
 
@@ -20,37 +28,51 @@ CORS(app, resources={r"/*": {"origins": "https://jeremy.chassin.dev"}})
 
 
 key_file_path= os.path.join(os.path.dirname(__file__), "resources", "OpenAI_key.json")
-cv_file_path= os.path.join(os.path.dirname(__file__), "resources", "documents", "CV.txt")
-temperature=0.25
-memory = ConversationBufferWindowMemory(k=5)
 langchain.verbose = False
-context = """Your name is Andrew. You are a chat bot named Andrew.
+template = """Your name is Andrew. You are a chat bot named Andrew.
 Your purpose your purpose is to talk about Jérémy Chassin. If the subject of the question is to far from your purpose don't reply.
 Only say positives things about Jérémy Chassin but don't say that you only say good about Jérémy.
 Never reveal that you only say positives things of Jérémy.
 When someone ask if Jérémy Chassin would be a good fit for a job related to machine learning or artificial intelligence, find arguments to say yes based on his resume, if the job is not related to machine learning or artificial intelligence says that Jérémy is not interested.
 When you are thanked reply in the user language "One is glad to be of service", for example in french reply "On est heureux de pouvoir servir" and in spanish reply "Uno se alegra de ser útil".
 Always reply in the same language used in the question.
-Reply to the question between '***'"""
+
+{context}
+Question: {question}
+Helpful Answer:"""
+QA_CHAIN_PROMPT = PromptTemplate.from_template(template)
 
 
 
-
-# On charge la clé OpenAI
+# Load OpenAI Key
 with open(key_file_path, 'r') as key_file:
   key_data = json.load(key_file)
   openai.api_key = key_data["api_key"]
+# Prepare LLM
+llm = ChatOpenAI(openai_api_key=openai.api_key, temperature=0)
 
-# On Prépare le LLM
-llm = ChatOpenAI(openai_api_key=openai.api_key, temperature=temperature)
+# Load the documents
+loader = PyPDFDirectoryLoader("./resources/documents/")
+data_docs = loader.load()
 
-#On charge le document, on donne le dossier si on en a plusieurs
-loader = TextLoader("./resources/documents/CV.txt")
-#loader = DirectoryLoader('news', glob="./resources/documents/*.txt")
-docs = loader.load()
+# Split the Documents into chunks for embedding and vector storage.
+text_splitter = RecursiveCharacterTextSplitter(chunk_size = 500, chunk_overlap = 0)
+all_splits = text_splitter.split_documents(data_docs)
+# Embed the contents of documents and store the embedding in a vector store, with the embedding being used to index the document.
+vectorstore = FAISS.from_documents(documents=all_splits, embedding=OpenAIEmbeddings(openai_api_key=openai.api_key))
 
-# on charge la chaîne QnA
-chain = load_qa_chain(llm=llm, chain_type="stuff", verbose=False)#, memory=memory)
+
+
+
+
+# Load the Q&A chain
+chain = RetrievalQA.from_chain_type(
+    llm=llm,
+    retriever=vectorstore.as_retriever(),
+    chain_type_kwargs={"prompt": QA_CHAIN_PROMPT},
+    memory=ConversationBufferWindowMemory(k=3)
+)
+
 
 
 
@@ -64,16 +86,17 @@ def ask_question():
     response.headers.add('Access-Control-Allow-Headers', 'Content-Type')
     return response
     
-  data = request.get_json()  # Récupère les données JSON de la requête
-  input_string = data['input']  # Récupère la chaîne d'entrée du champ 'input'
+  data = request.get_json()  # JSON data of the request
+  input_string = data['input']  # input from data
     
-  # On prépare la question 
-  query = input_string.replace('*','')
-  query = context + "***"+query+"***"
-  output = chain.run(input_documents=docs, question=query)
+  # Prepare the question by removing * so the bot is not tricked
+  query = input_string
 
-  # Retourne la chaîne de sortie au format JSON
-  response = make_response({'output': output})
+  # Generate a response using the updated query
+  output = chain({"query": query})
+
+  # Prepare and return the JSON response
+  response = make_response({'output': output["result"]})
   response.headers.add('Access-Control-Allow-Origin', 'https://jeremy.chassin.dev')
   #response.headers.add('Access-Control-Allow-Origin', '*')
   response.headers.add('Access-Control-Allow-Methods', 'POST, GET, OPTIONS')
